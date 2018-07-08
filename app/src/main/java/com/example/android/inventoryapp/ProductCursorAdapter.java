35168018e815049374d08e2738e6f35462e20ca0
Roland <roland.sarosy@gmail.com>
2018-07-08 21:15:49 +0200

    add functionality to the SALE button in the ListView to reduce the remaining quantity by 1

--- app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
@@ -1,10 +1,14 @@
 package com.example.android.inventoryapp;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
@@ -22,11 +26,12 @@ public class ProductCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        // Find reference to the views we are aiming to populate
+    public void bindView(View view, final Context context, final Cursor cursor) {
+        // Find reference to the views and buttons we'll use
         TextView productNameView = view.findViewById(R.id.list_item_product_name);
         TextView productPriceView = view.findViewById(R.id.list_item_product_price);
         TextView productQuantityView = view.findViewById(R.id.list_item_product_quantity);
+        Button sellButton = view.findViewById(R.id.list_item_sell_product);
 
         // Get the data from the cursor
         String productName = cursor.getString
@@ -38,11 +43,27 @@ public class ProductCursorAdapter extends CursorAdapter {
 
         // Add the extra strings to the price and quantity variables
         productPrice = context.getString(R.string.list_item_hint_unit_price) + " " + productPrice;
-        productQuantity = context.getString(R.string.list_item_hint_units_left) + " " + productQuantity;
+        String productQuantityString = context.getString(R.string.list_item_hint_units_left) + " " + productQuantity;
 
         // Populate the views with the data
         productNameView.setText(productName);
         productPriceView.setText(productPrice);
-        productQuantityView.setText(productQuantity);
+        productQuantityView.setText(productQuantityString);
+
+        // Decrease the number of the remaining quantity by one upon clicking on the Sell button
+        final int currentQuantity = Integer.valueOf(productQuantity);
+        final int currentProductId = cursor.getInt(cursor.getColumnIndex(InventoryContract.ProductsEntry._ID));
+        sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentQuantity > 0) {
+                    int newQuantity = currentQuantity - 1;
+                    Uri uri = ContentUris.withAppendedId(InventoryContract.ProductsEntry.CONTENT_URI, currentProductId);
+                    ContentValues values = new ContentValues();
+                    values.put(InventoryContract.ProductsEntry.COLUMN_PRODUCT_QUANTITY, newQuantity);
+                    context.getContentResolver().update(uri, values, null, null);
+                }
+            }
+        });
     }
 }
